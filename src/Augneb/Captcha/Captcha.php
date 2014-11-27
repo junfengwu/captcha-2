487bb9e58cddc7bdf9dadec44fb7ce1ecd711f87
tudou <augneb@gmail.com>
2014-11-27 22:22:22 +0800

    first commit

---
+++ src/Augneb/Captcha/Captcha.php
@@ -0,0 +1,20 @@
+<?php namespace Augneb\Captcha;
+
+// use Config, Str, Session, Hash, URL;
+
+/**
+ *
+ * A Simple Captcha Package for Laravel 4 
+ * 
+ * @version 1.0.0
+ * @link http://github.com/augneb/captcha
+ *
+ */
+
+class Captcha
+{
+    public static function info()
+    {
+        echo "This is captcha.";
+    }
+}
